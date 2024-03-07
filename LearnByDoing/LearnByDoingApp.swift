c217352ec0de29498b5ad62e07b56413dbdbce8c
Maya Ghamloush <maya.ghamloushe@gmail.com>
2024-03-07 10:20:43 +0100

    Initial Commit

---
+++ LearnByDoing/LearnByDoingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnByDoingApp.swift
+//  LearnByDoing
+//
+//  Created by Maya Ghamloush on 07/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct LearnByDoingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
