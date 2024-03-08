e2b77b6fe41920433ee0deae8464a3d3f2a282be
Maya Ghamloush <maya.ghamloushe@gmail.com>
2024-03-08 19:38:27 +0100

    Add Animation to card View

--- LearnByDoing/CardView.swift
+++ LearnByDoing/CardView.swift
@@ -9,12 +9,16 @@ import SwiftUI
 
 struct CardView: View {
     //MARK: - PROPERTIES
- 
+    @State private var fadeIn: Bool = false
+    @State private var moveDownard: Bool = false
+    @State private var moveUpward: Bool = false
+    
     var card: Card
     //MARK: - BODY
     var body: some View {
         ZStack {
             Image(card.imageName)
+                .opacity(fadeIn ? 1.0 : 0.0)
             
             VStack {
                 Text(card.title)
@@ -22,12 +26,13 @@ struct CardView: View {
                     .fontWeight(.heavy)
                     .foregroundColor(.white)
                     .multilineTextAlignment(.center)
+                
                 Text(card.headline)
                     .fontWeight(.light)
                     .foregroundColor(Color.white)
                     .italic()
             }
-            .offset(y:-218)
+            .offset(y: moveDownard ? -218 : -300)
             
             
           
@@ -55,12 +60,22 @@ struct CardView: View {
                     .shadow(color: .colorShadow, radius: 6, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 3)
                 })
       
-            .offset(y:218)
+                .offset(y: moveUpward ? 210 : 300)
         }//ZSTACK
         .frame(width: 335, height: 545, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
         .background(LinearGradient(gradient: Gradient(colors: card.gradientColors), startPoint: .top, endPoint: .bottom))
         .cornerRadius(16)
         .shadow(radius: 8)
+        .onAppear(){
+            withAnimation(.linear(duration: 1.2)){
+                self.fadeIn.toggle()
+            }
+            withAnimation(.linear(duration: 0.8)){
+                self.moveDownard.toggle()
+                self.moveUpward.toggle()
+            }
+            
+        }
     }
 }
 //MARK: - PREVIEW
